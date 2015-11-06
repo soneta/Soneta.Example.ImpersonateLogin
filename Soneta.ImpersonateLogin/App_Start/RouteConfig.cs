3f1d53165270af2c7c1244b6f254ba35cbf7ed89
Robert Szatkowski <robert.szatkowski@enova.pl>
2015-11-06 17:26:33 +0100

    Utworzenie projektu

---
+++ Soneta.ImpersonateLogin/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ImpersonateLoginExample {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Login365", // Route name
+                "Login365", // URL with parameters
+                new { controller = "Home", action = "Login365" } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Soneta.ImpersonateLogin/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.ServiceModel;
+
+namespace Soneta.ImpersonateLogin.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        [HttpPost,
+         AllowAnonymous]
+        public ActionResult Login365() {
+            ActionResult result = null;
+            var db = ConfigurationManager.AppSettings["enova365Db"];
+            var key = ConfigurationManager.AppSettings["enova365Key"];
+            var user = ConfigurationManager.AppSettings["enova365User"];
+            var url = ConfigurationManager.AppSettings["enova365Service"];
+
+            try {
+                var tokenInfo = new ServiceReference1.GenerateTokenInfo {
+                    User = user,
+                    DbName = db,
+                    Key = key
+                };
+
+                var binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+                var endpoint = new EndpointAddress(url + "/Business/BusinessProvider.svc");
+
+                var bus = new ServiceReference1.BusinessProviderClient(binding, endpoint);
+                var tokenResult = bus.GenerateToken(tokenInfo);
+                if (tokenResult.IsAuthenticated) {
+                    return new JsonNetResult(new {
+                        Token = tokenResult.Token,
+                        Db = db
+                    });
+                }
+
+                result = new JsonNetResult(new {
+                    ErrorHandle = "Niepoprawne dane logowania",
+                });
+            }
+            catch (Exception exc) {
+                result = new JsonNetResult(new { ErrorHandle = exc.Message });
+            }
+            return result;
+        }
+    }
+
+    public class JsonNetResult : ActionResult {
+        private object _obj { get; set; }
+        public JsonNetResult(object obj) {
+            _obj = obj;
+        }
+
+        public override void ExecuteResult(ControllerContext context) {
+            context.HttpContext.Response.AddHeader("content-type", "application/json");
+            context.HttpContext.Response.Write(JsonConvert.SerializeObject(_obj, Formatting.None, new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore }));
+        }
+    }
+
+}
