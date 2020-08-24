958ba1235ca91eb501949d7f13e2563b2a9f4d0c
Robert Szatkowski <robert.szatkowski@soneta.pl>
2020-08-24 09:25:14 +0200

    Dodanie obsługi protokołu Tls12

--- Soneta.ImpersonateLogin/Global.asax.cs
+++ Soneta.ImpersonateLogin/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ImpersonateLoginExample {
     public class MvcApplication : System.Web.HttpApplication {
         protected void Application_Start() {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
